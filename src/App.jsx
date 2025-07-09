d726e5e77fd2d973027de752b35e84c1946ce65b
Calet Devia <calebtius@gmail.com>
2025-07-09 12:08:28 -0500

    Added the structure to study Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 function App() {
-  return (
-    <>
-      <div className="flex justify-center gap-2 bg-blue-500 animate-bounce p-2 rounded-3xl w-full cursor-pointer transition-colors hover:bg-amber-200  ">
-        <p className="text-white w-fit">Hola</p>
-        <p className="text-white w-fit">Hola</p>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "100%",
-          backgroundColor: "red",
-        }}
-      >
-        <p className="text-white w-fit">Hola</p>
-        <p className="text-white w-fit">Hola</p>
-      </div>
-    </>
-  );
+  return <>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, error? Dolore delectus dignissimos laborum culpa cumque soluta repellendus, rem possimus inventore ullam dolores sint voluptatum quo quasi harum unde beatae.</p>
+  </>;
 }
 
 export default App;
