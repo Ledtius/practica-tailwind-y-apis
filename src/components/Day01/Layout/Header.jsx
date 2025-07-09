352f9310bb418f801b2ba5e809dd3d66553c91b5
Calet Devia <calebtius@gmail.com>
2025-07-09 18:51:16 -0500

    Fixed the header margin (general margin)

--- src/components/Day01/Layout/Header.jsx
+++ src/components/Day01/Layout/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <header className="font-bold w-full flex flex-col items-center gap-2 p-2 text-center mb-4">
+      <header className="font-bold w-full flex flex-col items-center gap-2 p-2 text-center m-4">
         <h1 className="text-4xl">¡Amos de la conquista!</h1>
         <h2 className="text-base/tight font-semibold">Conquistadores de la epoca antigua</h2>
       </header>
