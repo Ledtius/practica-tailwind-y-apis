81a7508d03403c72055a1ef91a9daa975c2f74ec
Calet Devia <calebtius@gmail.com>
2025-07-10 18:34:43 -0500

    Working with grid

--- src/components/Day02/Layout/Main.jsx
+++ src/components/Day02/Layout/Main.jsx
@@ -1,8 +1,32 @@
 const Main = () => {
   return (
     <>
-      <main>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla perspiciatis tempore sint fuga modi assumenda vero. Illo, tempora quis quo eum at perspiciatis delectus! Laborum nostrum voluptates illo nulla laudantium.
+      <main className="grid grid-cols-3 gap-2 justify-between bg-gray-500  w-full h-full p-2.5">
+        <img
+          className="w-3xs h-96 object-cover rounded-2xl"
+          src="https://images2.alphacoders.com/133/thumb-1920-1332614.jpeg"
+          alt="Modekaiser"
+        />
+        <img
+          className="w-3xs h-96 object-cover rounded-2xl"
+          src="https://images8.alphacoders.com/135/thumb-1920-1352312.jpeg"
+          alt="Master Chief"
+        />
+        <img
+          className="w-3xs h-96 object-cover rounded-2xl"
+          src="https://images3.alphacoders.com/135/thumb-1920-1353838.png"
+          alt="Minecraft"
+        />
+        <img
+          className="w-3xs h-96 object-cover rounded-2xl"
+          src="https://picfiles.alphacoders.com/469/thumb-1920-469372.jpg"
+          alt="GtaIV"
+        />
+        <img
+          className="w-3xs h-96 object-cover rounded-2xl"
+          src="https://images8.alphacoders.com/284/thumb-1920-284927.jpg"
+          alt="Metro2033"
+        />
       </main>
     </>
   );
