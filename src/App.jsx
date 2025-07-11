f095a48c1b1fb5ec8e3e7343f94c0922c8c40c9e
Calet Devia <calebtius@gmail.com>
2025-07-11 14:22:18 -0500

    callback exe6 but now with promise

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,46 @@ import Footer from "./components/Day02/Layout/Footer.jsx";
 function App() {
   const user = { id: 123, name: "Calet" };
 
-  const getUser = (obj, callback) => {
+  const getUser = (userObj) => {
     console.log("Finding the user...");
-    setTimeout(() => {
-      callback(obj);
-    }, 1000);
+    return new Promise((resolves, reject) => {
+      setTimeout(() => {
+        resolves(userObj);
+      }, 1000);
+    });
   };
 
-  const getPostsByUser = ({ id }, callback) => {
+  const getPostsByUser = ({ id }) => {
     const posts = ["post1", "post2", "post3"];
 
-    setTimeout(() => {
-      console.log(`ID of user: ${id}`);
-      callback(posts);
-      return posts;
-    }, 1500);
+    return new Promise((resolves, reject) => {
+      setTimeout(() => {
+        console.log(`ID of user: ${id}`);
+        resolves(posts);
+      }, 1500);
+    });
   };
 
   const getPosts = (postElements) => {
-    console.log(postElements);
+    return new Promise((resolves, reject) => {
+      resolves(postElements);
+    });
   };
 
-  getUser(user, (userReceive) => {
-    getPostsByUser(userReceive, (postsUser) => {
-      getPosts(postsUser);
-    });
-  });
+  getUser(user)
+    .then((value) => {
+      return getPostsByUser(value);
+    })
+    .then((value) => {
+      return getPosts(value);
+    })
+    .then((value) => console.log(value));
+
+  // getUser(user, (userReceive) => {
+  //   getPostsByUser(userReceive, (postsUser) => {
+  //     getPosts(postsUser);
+  //   });
+  // });
 
   return (
     <>
