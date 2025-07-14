fd888f3b0d901e613b1ca33229d05cfaf174b516
Calet Devia <calebtius@gmail.com>
2025-07-14 17:30:15 -0500

    Finished exe1day3 tcss (for real)

--- src/components/Day03/Layout/Main.jsx
+++ src/components/Day03/Layout/Main.jsx
@@ -1,72 +1,29 @@
 const Main = () => {
   return (
     <>
-      <main className="mt-30 h-dvh flex flex-col items-center justify-center gap-3 w-full">
-        <section className="custom-alert bg-green-300">
-          <svg
-            className="custom-svg fill-green-600"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
-          </svg>
-          <header className="custom-alert-text ring-green-700">
-            <h3 className="text-lg font-semibold mr-1.5">Success:</h3>
-            <span className="w-full mr-14">Updated menber status</span>
+      <main className="mt-30 h-dvh p-5 flex flex-col items-center justify-center gap-3 w-full bg-blue-400">
+        <section className="flex flex-col items-center max-w-2xs shadow-2xl">
+          <header className="bg-gray-100 w-full h-52 rounded-tl-2xl  rounded-tr-2xl flex justify-center relative ">
+            <div className="absolute top-8 rounded-full p-1 ring-3 ring-red-400 shadow-xl">
+              <img
+                className="object-cover h-34 w-34 rounded-full  "
+                src="https://plus.unsplash.com/premium_photo-1689551670902-19b441a6afde?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
           </header>
-          <button className="cursor-pointer">
-            <svg
-              className="custom-svg2"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-            >
-              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-            </svg>
-          </button>
-        </section>
-        <section className="custom-alert bg-orange-300">
-          <svg
-            className="custom-svg fill-orange-600"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24l0 112c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-112c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-          </svg>
-          <header className="custom-alert-text ring-orange-700">
-            <h3 className="text-lg font-semibold mr-1.5">Warning:</h3>
-            <span className="w-full mr-14">User has to be admin</span>
-          </header>
-          <button className="cursor-pointer">
-            <svg
-              className="custom-svg2"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-            >
-              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-            </svg>
-          </button>
-        </section>
-        <section className="custom-alert bg-red-300">
-          <svg
-            className="custom-svg fill-red-600"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z" />
-          </svg>
-          <header className="custom-alert-text ring-red-700">
-            <h3 className="text-lg font-semibold mr-1.5">Error:</h3>
-            <span className="w-full mr-14">Internal Server Error</span>
-          </header>
-          <button className="cursor-pointer">
-            <svg
-              className="custom-svg2"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-            >
-              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-            </svg>
-          </button>
+          <article className="flex flex-col items-center justify-between max-w-2xs gap-5 p-5 bg-red-400 rounded-bl-2xl rounded-br-2xl">
+            <header className="text-center">
+              <h2 className="text-2xl font-bold ">Ana Liz</h2>
+              <span className="text-md font-semibold ">
+                Designer and artist
+              </span>
+            </header>
+
+            <p className=" text-justify text-gray-800">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </article>
         </section>
       </main>
     </>
