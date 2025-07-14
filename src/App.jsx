45620fcd64e6afcc0793af0f92c6f6fff1776971
Calet Devia <calebtius@gmail.com>
2025-07-14 15:31:35 -0500

    exe10

--- src/App.jsx
+++ src/App.jsx
@@ -5,87 +5,28 @@ import Main from "./components/Day03/Layout/Main.jsx";
 import Footer from "./components/Day02/Layout/Footer.jsx";
 
 function App() {
-  const products = [
-    { id: 1, name: "mouse", price: 12000, category: "accessories" },
-    { id: 2, name: "keyboard", price: 25000, category: "accessories" },
-    { id: 3, name: "monitor", price: 75000, category: "screens" },
-    { id: 4, name: "laptop", price: 220000, category: "computers" },
-  ];
-
-  /* Callback form */
-
-  const getProduct = (productName, callback) => {
-    const productFound = products.find(({ name }) => name === productName);
-
-    console.log("Processing name...");
-
-    setTimeout(() => {
-      if (productName) callback(productFound);
-      else callback(undefined);
-    }, 1000);
+  const two = (resp) => {
+    return resp.json();
   };
 
-  const printProduct = (product) => {
-    if (product)
-      setTimeout(() => {
-        const { name, category, price } = product;
-
-        console.log(
-          `Product found:\nProduct name: ${name}\nCategory: ${category}\nPrice: $${price} `
-        );
-      }, 1000);
-    else console.log("Error found the product");
-  };
-
-  // getProduct("mouse", printProduct);
-
-  /* Promise form */
-
-  function getProductPromise(productName) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const productFound = products.find(({ name }) => name === productName);
-
-        if (productFound) resolve(productFound);
-        else reject("Error, product not found");
-      }, 1000);
-    });
-  }
-
-  function printProductPromise(product) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (product) {
-          const { name, category, price } = product;
-
-          resolve(
-            `Product found:\nProduct name: ${name}\nCategory: ${category}\nPrice: $${price}`
-          );
-        } else reject("Error printed the product");
-      }, 1000);
-    });
-  }
-
-  getProductPromise("monitor")
-    .then((productFound) => {
-      printProductPromise(productFound);
-    })
-    .catch((e) => console.error(e));
-
-  /* Async / Await form */
-
-  async function printProductAsyncAway(productName) {
+  async function proveSomething() {
     try {
-      const findProduct = await getProductPromise(productName);
+      const one = await fetch("https://api.ipify.org?format=json");
 
-      const printProduct = await printProductPromise(findProduct);
-      console.log(printProduct);
+      const ttwo = await two(one);
+
+      console.log(ttwo);
     } catch (e) {
       console.error(e);
     }
   }
 
-  printProductAsyncAway("laptop");
+  proveSomething();
+
+  fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((e) => console.error(e));
 
   return (
     <>
