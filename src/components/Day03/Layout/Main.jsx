56ac6f2121cc217ef0ca9adb59f0f8f14b0e53b0
Calet Devia <calebtius@gmail.com>
2025-07-14 16:12:49 -0500

    Finished exe1day3 tcss

--- src/components/Day03/Layout/Main.jsx
+++ src/components/Day03/Layout/Main.jsx
@@ -1,16 +1,73 @@
 const Main = () => {
   return (
     <>
-      <main className="mt-30 h-dvh flex gap-3">
-        <a href="#" className="btns-custom bg-blue-500">
-          Button #1
-        </a>
-        <a href="#" className="btns-custom bg-red-400">
-          Button #2
-        </a>
-        <a href="#" className="btns-custom bg-green-400">
-          Button #3
-        </a>
+      <main className="mt-30 h-dvh flex flex-col items-center justify-center gap-3 w-full">
+        <section className="custom-alert bg-green-300">
+          <svg
+            className="custom-svg fill-green-600"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+          </svg>
+          <header className="custom-alert-text ring-green-700">
+            <h3 className="text-lg font-semibold mr-1.5">Success:</h3>
+            <span className="w-full mr-14">Updated menber status</span>
+          </header>
+          <button className="cursor-pointer">
+            <svg
+              className="custom-svg2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            </svg>
+          </button>
+        </section>
+        <section className="custom-alert bg-orange-300">
+          <svg
+            className="custom-svg fill-orange-600"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24l0 112c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-112c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+          </svg>
+          <header className="custom-alert-text ring-orange-700">
+            <h3 className="text-lg font-semibold mr-1.5">Warning:</h3>
+            <span className="w-full mr-14">User has to be admin</span>
+          </header>
+          <button className="cursor-pointer">
+            <svg
+              className="custom-svg2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            </svg>
+          </button>
+        </section>
+        <section className="custom-alert bg-red-300">
+          <svg
+            className="custom-svg fill-red-600"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z" />
+          </svg>
+          <header className="custom-alert-text ring-red-700">
+            <h3 className="text-lg font-semibold mr-1.5">Error:</h3>
+            <span className="w-full mr-14">Internal Server Error</span>
+          </header>
+          <button className="cursor-pointer">
+            <svg
+              className="custom-svg2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            </svg>
+          </button>
+        </section>
       </main>
     </>
   );
