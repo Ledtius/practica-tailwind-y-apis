1cbe460a8c4ed3ee25a781970743322049edf083
Calet Devia <calebtius@gmail.com>
2025-07-16 22:35:41 -0500

    Added interactivity in the hamburger menu exe1day5 tcss

--- src/components/Day05/Layout/Header.jsx
+++ src/components/Day05/Layout/Header.jsx
@@ -1,18 +1,18 @@
+import { useState } from "react";
 function Header() {
+  const [showNav, setShowNav] = useState(false);
+
   return (
     <>
-      <header className="flex justify-between w-full p-4 bg-gray-500 shadow-2xl/5">
-        <section className="flex items-center">
+      <header className="flex flex-row-reverse bg-slate-900 justify-between w-full p-4 bg- shadow-2xl/5 relative">
+        <a href="#" className="flex items-center size-fit">
           <svg
-            className="stroke-red-500 stroke-2 size-12"
+            className="stroke-green-400 stroke-2 size-12"
             xmlns="http://www.w3.org/2000/svg"
             width="28"
             height="28"
             viewBox="0 0 24 24"
             fill="none"
-            stroke-width="0.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
           >
             <path d="M11 17a2.5 2.5 0 0 0 2 0" />
             <path d="M12 3c-4.664 0 -7.396 2.331 -7.862 5.595a11.816 11.816 0 0 0 2 8.592a10.777 10.777 0 0 0 3.199 3.064c1.666 1 3.664 1 5.33 0a10.777 10.777 0 0 0 3.199 -3.064a11.89 11.89 0 0 0 2 -8.592c-.466 -3.265 -3.198 -5.595 -7.862 -5.595z" />
@@ -20,38 +20,80 @@ function Header() {
             <path d="M16 11l-2 2" />
           </svg>
 
-          <h2 className="font-bold text-2xl">Ius</h2>
-        </section>
+          <h2 className="font-bold text-2xl text-green-100 hidden ">Ius</h2>
+        </a>
 
-        <nav className="hidden">
-          <ul>
+        <nav
+          className={
+            showNav
+              ? `fixed left-0 button-0 top-0 bg-slate-900 text-gray-200 w-5/6 h-screen text-2xl font-semibold pl-4 pt-4 flex flex-col gap-5`
+              : ` hidden`
+          }
+        >
+          <button
+            className="fill-gray-300"
+            onClick={() => {
+              setShowNav(false);
+            }}
+          >
+            <svg
+              className="size-12"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            </svg>
+          </button>
+
+          <ul className="flex flex-col gap-6">
             <li>
-              <a href="#">Home</a>
+              <a
+                href="#"
+                onClick={() => {
+                  console.log("sss");
+                  setShowNav(false);
+                }}
+              >
+                Home
+              </a>
             </li>
             <li>
-              <a href="#">Products</a>
+              <a
+                href="#"
+                onClick={() => {
+                  console.log("sss");
+                  setShowNav(false);
+                }}
+              >
+                Products
+              </a>
             </li>
             <li>
-              <a href="#">About us</a>
+              <a
+                href="#"
+                onClick={() => {
+                  console.log("sss");
+                  setShowNav(false);
+                }}
+              >
+                About us
+              </a>
             </li>
           </ul>
         </nav>
-        <button className="">
+        <button
+          className="cursor-pointer"
+          onClick={() => {
+            console.log("sss");
+            setShowNav(true);
+          }}
+        >
           <svg
-            className="size-10"
+            className="size-10 fill-gray-300"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 448 512"
           >
             <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
-          </svg>
-        </button>
-        <button className="hidden">
-          <svg
-            className="size-12"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-          >
-            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
           </svg>
         </button>
       </header>
